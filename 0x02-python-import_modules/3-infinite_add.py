1046b1deb7505e64547a0acdb0538cb235951321
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-03-14 19:18:17 -0700

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     """ program that prints the result of the addition of all arguments
     The output should be the result of the addition of all arguments, followed by a new line
     You can cast arguments into integers by using int() (you can assume that all arguments can be casted into integers)
-    Your code should not be executed when imported
+    your code should not be executed when imported
     """
     import sys
     addition_total = 0
