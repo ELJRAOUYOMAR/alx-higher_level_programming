861e02ca8e5084855c1b7eff7bb112011b8f266f
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-04-20 05:33:13 -0700

    add line for pep8

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,6 +2,7 @@
 """Defines a function to write a string to a text file and return the number of characters written."""
 
 
+
 def write_file(filename="", text=""):
     """Writes the given text to the specified filename and returns the number of characters written."""
     with open(filename, 'w', encoding='utf-8') as f:
