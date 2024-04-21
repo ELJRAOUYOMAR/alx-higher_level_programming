fe04c079e9dfbd7e119a1e80f560a6f271fdb51f
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-04-21 03:34:13 -0700

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Defines a function to write a string to a text file and return the number of characters written."""
+"""Defines a function to write a string to a text file and
+return the number of characters written."""
 
 
 def write_file(filename="", text=""):
-    """Writes the given text to the specified filename and returns the number of characters written."""
+    """Writes the given text to the specified filename and 
+    returns the number of characters written."""
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
