75bc9dda2313ceb72a8ae717df1159d8f42151f5
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-04-22 17:08:08 +0100

    Task 2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,66 @@
 #!/usr/bin/python3
 """Rectangle class"""
-from base import Base
+from models.base import Base
 
 class Rectangle(Base):
     """Rectangle class"""
+    
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor"""
+
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+    
+    @property
+    def width(self):
+        """get the  width of this rectangle"""
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        """set a value to the width of this rectangle"""
+        self.__width = value
+
+    @property
+    def height(self):
+        """get the  height of this rectangle"""
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        """set a value to the height of this rectangle"""
+        self.__height = value
+
+    @property
+    def x(self):
+        """get the  x of this rectangle"""
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        """set a value to the x of this rectangle"""
+        self.__x = value
+
+    @property
+    def y(self):
+        """get the  y of this rectangle"""
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        """set a value to the y of this rectangle"""
+        self.__y = value
+
+    
+
+    
+    
+
+    
+
+
+    
     
