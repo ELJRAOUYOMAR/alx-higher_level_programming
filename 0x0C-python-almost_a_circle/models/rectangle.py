15eaf5eb202efa48c8149d2f11659b6db1e3fd23
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-04-22 19:14:46 +0100

    8. Update #0

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -100,6 +100,19 @@ class Rectangle(Base):
                        self.x, self.y, 
                        self.width, self.height) 
 
+    def update(self, *args):
+        '''Updates instance attributes via no-keyword & keyword args.'''
+
+        if args:
+            self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
     
     
 
