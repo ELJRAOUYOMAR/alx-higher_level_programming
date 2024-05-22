7205d963a765ed46263775b93cd76ab955b5c427
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-05-22 16:46:13 -0700

    1. Root user

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -4,5 +4,12 @@
 -- 2)The user_0d_1 password should be set to user_0d_1_pwd
 -- 3)If the user user_0d_1 already exists, your script should not fail
 
--- cat 1-create_user.sql | mysql -hlocalhost -uroot -p
--- cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+-- Create user 'user_0d_1' with all privileges
+-- Password for user set as 'user_0d_1_pwd'
+-- If user already exists, script should not fail
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.*
+TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
+
