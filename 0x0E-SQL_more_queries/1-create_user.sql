3836993f9b9e0132d54c08f248578f453d9c7d18
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-05-22 16:42:14 -0700

    0. My privileges!

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- script that creates the MySQL server user user_0d_1.
+
+-- 1)user_0d_1 should have all privileges on your MySQL server
+-- 2)The user_0d_1 password should be set to user_0d_1_pwd
+-- 3)If the user user_0d_1 already exists, your script should not fail
+
+-- cat 1-create_user.sql | mysql -hlocalhost -uroot -p
+-- cat 0-privileges.sql | mysql -hlocalhost -uroot -p
