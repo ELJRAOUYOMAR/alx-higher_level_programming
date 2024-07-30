425b0f48a7814627bcdec30d6bde3b4294040ca8
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-07-30 09:09:58 -0700

    -

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 const path = process.argv[2];
 
-fs.readeFile(path, 'utf-8', (error, data) => {
+fs.readFile(path, 'utf-8', (error, data) => {
     if (error) {
         console.log(error);
         return;
