6041cfd845525464d5d5f195ea929ee8e279dc3c
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-07-30 13:25:16 -0700

    -

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,6 +2,6 @@
 
 const request = require('request');
 
-request(process.argv[2], function (_error, response) {
-    console.log("code:", response.statusCode);
+request(process.argv[2], function (_error, res) {
+  console.log('code:', res.statusCode);
 });
