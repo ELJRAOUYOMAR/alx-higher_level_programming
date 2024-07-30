4c77b7d9b703d8345d992f338235ef2c5eb73966
ELJRAOUYOMAR <etudecompte78@gmail.com>
2024-07-30 23:56:36 +0100

    -

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const request = require('request');
+const path = 'https://swapi-api.alx-tools.com/api/films/'+ process.argv[2];
+
+request(path, function (error, response, data) {
+    data = JSON.parse(data);
+  console.log(body.title);
+});
